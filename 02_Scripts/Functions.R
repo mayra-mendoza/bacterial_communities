a08051a3092c40f6514a82ced17965b724db4b73
mayra-mendoza <mayra.mendozavelazquez@gmail.com>
2026-04-21 11:52:45 -0600

    Arranging

--- 02_Scripts/Functions.R
+++ 02_Scripts/Functions.R
@@ -4,6 +4,60 @@
 # Stan functions ---------------------------------------------------------------
 
 # 1. Initial values standarized 
+
+
+# Stan function for logistic LV model fitting 
+loglv <- '
+functions {
+  vector lvfnc(real t,           // time
+               vector z,         // state (OD in this particular case)
+               real r,           // r
+               real k) {         // k
+               
+    vector[3] dzdt;
+    for (j in 1:3) {
+      dzdt[j] = r * z[j] * (1 - z[j] / k);
+    }
+    return dzdt;
+  }
+}
+
+data {
+  int<lower=1> N;            
+  real ts[N];                // time series [without the first time measure]
+  vector<lower=0>[3] y0;     // observed initial state 
+  real<lower=0> y[N, 3];     // population measures
+}
+
+parameters {
+  real<lower=0> r;
+  real<lower=0> k;
+  vector<lower=0>[3] z0;     // initial state (estimation)
+  real<lower=0> sigma;
+}
+
+transformed parameters {
+  // using vectors instead of real[]
+  vector[3] z[N] = ode_rk45(lvfnc, z0, 0.0, ts, r, k);
+}
+
+model {
+  // Priors
+  r ~ normal(0.3, 0.05);
+  k ~ normal(0.8, 0.05);
+  sigma ~ lognormal(0, 0.05); # low error 
+
+  // Prior for the initial state based on the values
+  z0 ~ normal(y0, 0.1);
+  
+  // Likelihood
+  for (j in 1:3) {
+    y[, j] ~ normal(z[, j], sigma); 
+  }
+}
+'
+
+# For standarized initial values 
 stan_initial_standarized <- function(){
   list(
     r = 0.3,
@@ -12,5 +66,96 @@ stan_initial_standarized <- function(){
     sigma = 0.1
   )
 }
+
+# Stan function with the Log LV model 
+stan_ccfunct <- function (df, temp_col, replica_col, strain_col, interest_col, time_series, time_alternative, niterations, nchains){
+  
+  # assigning objects to specific values in the data.frame 
+  
+  spps <- unique(df[[strain_col]]) 
+  ntemps <- sort(unique(df[[temp_col]]), decreasing = FALSE)
+  
+  ntemps_numeric <- as.numeric(ntemps) 
+  ntemps_character <- as.character(ntemps)
+  nreplica <- unique(df[[replica_col]])
+  
+  vector_freplica <- list() 
+  p <- 1
+  
+  for (m in 1:length(spps)) {
+    for (o in 1:length(nreplica)) {
+      
+      df_complete <- df[df[[strain_col]] == spps[m] & 
+                          df[[replica_col]] == nreplica[o] & 
+                          df[[temp_col]] %in% ntemps_numeric, ]
+      
+      if (nrow(df_complete) > 0){
+        
+        df_filtered <- df_complete %>% 
+          arrange(.data[[temp_col]]) %>%
+          pull(.data[[interest_col]])
+        
+        df_matrix <- matrix(df_filtered, ncol = length(ntemps_numeric))
+        colnames(df_matrix) <- ntemps_character
+        
+        vector_freplica[[p]] = df_matrix
+        
+        names(vector_freplica)[p] <- paste0(spps[m], "_rep", nreplica[o])
+        p <- p + 1
+      }
+    }
+  }
+  
+  
+  # Generating initial values and every data.frame for the stan input 
+  init_v <- list()
+  m_data <- list()
+  stan_input <- list()
+  ts_vector <- list()
+  
+  for (q in seq_along(vector_freplica)) {
+    
+    # extract the first row of every data.frame to get the initial values for stan
+    init_v <- as.numeric(vector_freplica[[q]][1, ])
+    
+    # Extract the rest of the data 
+    m_data[[q]] <- vector_freplica[[q]][-1, ]
+    
+    # if - to identify specific variations in the data.frame for the time_series 
+    
+    if (nrow(vector_freplica[[q]]) == length(time_alternative)) {
+      
+      ts_vector <- time_alternative[-1] 
+      
+    } else {
+      
+      ts_vector <- time_series[-1]
+      
+    }
+    
+    # That way it doesn't matter if we have another time_series alternative 
+    
+    # Create the stan input 
+    stan_input[[names(vector_freplica)[q]]] <- list(
+      N = nrow(m_data[[q]]), 
+      ts = ts_vector, 
+      y0 = init_v,
+      y  = m_data[[q]]
+    )
+  }
+  
+  stan_output <- list()
+  for (r in seq_along(stan_input)){
+    stan_output[[r]] <- stan(model_code = loglv, # here it is the stan function i created earlier
+                             data = stan_input[[r]], # stan_input 
+                             save_dso = FALSE, 
+                             iter = niterations,  # iterations
+                             chains = nchains,   # n. chains 
+                             init = stan_initial_standarized ) # this function is available in the "Functions" script
+    
+  }
+  return(stan_output)
+}
+
 
 # Reconstruction methods -------------------------------------------------------
